e110796207ecb5a24888b96565ecbead095d7352
Roger John William Hobbs <rhobbs@student.wethinkcode.co.za>
2020-09-16 19:25:46 +0200

    init

---
+++ fixme-router/src/main/java/fourtytwo/Router.java
@@ -0,0 +1,13 @@
+package fourtytwo;
+
+/**
+ * Hello world!
+ *
+ */
+public class Router
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello from router" );
+    }
+}
