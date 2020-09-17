4be0d36e62165fe72a8e0036ffe9da24a6035b44
Roger John William Hobbs <rhobbs@student.wethinkcode.co.za>
2020-09-17 12:11:34 +0200

    Added second module, basic parent child module set working with pom

---
+++ fixme-broker/src/main/java/fourtytwo/Broker.java
@@ -0,0 +1,13 @@
+package fourtytwo;
+
+/**
+ * Hello world!
+ *
+ */
+public class Broker
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
