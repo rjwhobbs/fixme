5d221c713dd0cc301d3cd855d5469a075c0fc595
Martian1431 <pmalope@student.wethinkcode.co.za>
2020-10-12 21:21:55 +0200

    Refactoring the code, hope I didn't break anything. Is a lot to take in...

--- fixme-router/src/main/java/fortytwo/ClientAttachment.java
+++ fixme-router/src/main/java/fortytwo/ClientAttachment.java
@@ -1,4 +1,15 @@
 package fortytwo;
 
-public class ClientAttachment {
+import java.nio.ByteBuffer;
+import java.nio.channels.AsynchronousSocketChannel;
+
+final class ClientAttachment {
+    ByteBuffer buffer = ByteBuffer.allocate(4096);
+    AsynchronousSocketChannel client;
+    String id;
+
+    ClientAttachment(AsynchronousSocketChannel client, String id) {
+        this.client = client;
+        this.id = id;
+    }
 }
--- fixme-router/src/main/java/fortytwo/Router.java
+++ fixme-router/src/main/java/fortytwo/Router.java
@@ -1,18 +1,9 @@
 package fortytwo;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.InetSocketAddress;
-import java.nio.channels.AsynchronousServerSocketChannel;
-import java.nio.channels.AsynchronousSocketChannel;
-import java.nio.channels.CompletionHandler;
-import java.util.HashMap;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
-import java.util.regex.Pattern;
 
-public class Router {
+final public class Router {
     public static void main( String[] args ) {
         final Server server = new Server();
         Executor pool = Executors.newFixedThreadPool(2);
@@ -28,90 +19,5 @@ public class Router {
                 server.acceptMarket();
             }
         });
-    }
-}
-
-class Server {
-    private static BufferedReader blockerReader;
-    private static Pattern pattern;
-    private static Executor pool;
-    private static int brokersIndex;
-    private static int marketsIndex;
-    private static HashMap<String, ClientAttachment> brokers;
-    private static HashMap<String, ClientAttachment> markets;
-
-    public Server() {
-        blockerReader = new BufferedReader(new InputStreamReader(System.in));
-        pattern = Pattern.compile("^\\\\(\\d+)\\s+(.+)");
-        pool = Executors.newFixedThreadPool(200);
-        brokers = new HashMap<>();
-        markets = new HashMap<>();
-        brokersIndex = 1;
-        marketsIndex = 1;
-    }
-
-    public void acceptBroker() {
-        try (final AsynchronousServerSocketChannel brokerChannel = AsynchronousServerSocketChannel.open()) {
-            InetSocketAddress hostAddress = new InetSocketAddress("localhost", 5000);
-            brokerChannel.bind(hostAddress);
-            while (true) {
-                brokerChannel.accept(null, new CompletionHandler<AsynchronousSocketChannel, Object>() {
-                    @Override
-                    public void completed(AsynchronousSocketChannel result, Object attachment) {
-                        if (result.isOpen()) {
-                            brokerChannel.accept(null, this);
-                            System.out.println("Broker Connected");
-                        }
-                    }
-
-                    @Override
-                    public void failed(Throwable exc, Object attachment) {
-
-                    }
-                });
-                System.out.println("Listening on port 5000");
-                blocker();
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void acceptMarket() {
-        try (final AsynchronousServerSocketChannel marketChannel = AsynchronousServerSocketChannel.open()) {
-            InetSocketAddress hostAddress = new InetSocketAddress("localhost", 5001);
-            marketChannel.bind(hostAddress);
-
-            while (true) {
-                marketChannel.accept(null, new CompletionHandler<AsynchronousSocketChannel, Object>() {
-                    @Override
-                    public void completed(AsynchronousSocketChannel result, Object attachment) {
-                        if (result.isOpen()) {
-                            marketChannel.accept(null, this);
-                            System.out.println("Market Connected");
-                        }
-                    }
-
-                    @Override
-                    public void failed(Throwable exc, Object attachment) {
-                        System.out.println("Something went wrong");
-                    }
-                });
-                System.out.println("Listening on port 5001");
-                blocker();
-            }
-        } catch (Exception e) {
-
-        }
-    }
-
-    public static void blocker() {
-        try {
-            blockerReader.readLine();
-            blocker();
-        }
-        catch (IOException e) {
-            System.out.println(e.getMessage());
-        }
     }
 }
